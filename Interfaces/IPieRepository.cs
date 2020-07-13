5db78979703c55388d8a38eb474364511962f8e3
harpalgupta <harpal.gupta@gmail.com>
2020-07-13 16:27:39 +0100

    first commit

---
+++ Interfaces/IPieRepository.cs
@@ -0,0 +1,13 @@
+using Models;
+using System.Collections.Generic;
+
+namespace BethanysPieShop.Interfaces
+{
+    public interface IPieRepository
+    {
+        public IEnumerable<Pie> AllPies { get; }
+        public IEnumerable<Pie> PiesOfTheWeek { get;  }
+        public Pie GetPieById(int Id);
+        
+    }
+}
---
+++ Repository/MockCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using BethanysPieShop.Interfaces;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Repository
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category> {
+            new Category{ Id=1,CategoryName="FruitPies",Description="all fruity Pies"},
+            new Category{ Id=1,CategoryName="Cheese Cakes",Description="all cheese cakes"},
+            new Category{ Id=1,CategoryName="Seasonal Pies",Description="all seasonal pies"},
+            };
+    }
+}
